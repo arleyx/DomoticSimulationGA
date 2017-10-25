7f188a08464d336b21c1572172609ceafa95e46d
Arley Archila <arleyarchilaherreno@gmail.com>
2017-10-24 23:09:02 -0500

    Add script file to generate stage config files

---
+++ sh_generate.sh
@@ -0,0 +1,26 @@
+# CREAR ARCHIVOS DE CONFIGURACIÓN
+
+# STAGE 1
+java -jar Simulador.jar -f Stages/stage1/case1/setup Stages/stage1/case1/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage1/case2/setup Stages/stage1/case2/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage1/case3/setup Stages/stage1/case3/machines/User/User.xml
+
+# STAGE 2
+java -jar Simulador.jar -f Stages/stage2/case1/setup Stages/stage2/case1/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage2/case2/setup Stages/stage2/case2/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage2/case3/setup Stages/stage2/case3/machines/User/User.xml
+
+# STAGE 3
+java -jar Simulador.jar -f Stages/stage3/case1/setup Stages/stage3/case1/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage3/case2/setup Stages/stage3/case2/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage3/case3/setup Stages/stage3/case3/machines/User/User.xml
+
+# STAGE 4
+java -jar Simulador.jar -f Stages/stage4/case1/setup Stages/stage4/case1/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage4/case2/setup Stages/stage4/case2/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage4/case3/setup Stages/stage4/case3/machines/User/User.xml
+
+# STAGE 5
+java -jar Simulador.jar -f Stages/stage5/case1/setup Stages/stage5/case1/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage5/case2/setup Stages/stage5/case2/machines/User/User.xml
+java -jar Simulador.jar -f Stages/stage5/case3/setup Stages/stage5/case3/machines/User/User.xml
